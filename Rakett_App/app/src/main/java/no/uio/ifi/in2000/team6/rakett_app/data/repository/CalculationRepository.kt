9bc5ad7935915e98549083add5e5561bcd2720c7
ibrah <ibrahinu@ulrik.uio.no>
2025-03-25 20:50:24 +0100

    Oppdaterte .gitignore filen

--- Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/data/repository/CalculationRepository.kt
+++ Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/data/repository/CalculationRepository.kt
@@ -1,33 +1,33 @@
-package no.uio.ifi.in2000.team6.rakett_app.data.repository
+    package no.uio.ifi.in2000.team6.rakett_app.data.repository
 
-import kotlin.math.atan2
-import kotlin.math.sqrt
+    import kotlin.math.atan2
+    import kotlin.math.sqrt
 
-class CalculationRepository {
+    class CalculationRepository {
 
-    // Regner ut hvor sterk vinden er basert på øst-vest og nord-sør retning
-    fun calculateWindSpeed(u: Double, v: Double): Double {
-        // u er vindstyrke i øst-vest retning, v er vindstyrke i nord-sør retning
-        // Bruker pytagoras for å regne ut total vindstyrke
-        return sqrt(u * u + v * v)
-    }
-
-    // Regner ut hvilken retning vinden kommer fra i grader (0-360)
-    fun calculateWindDirection(u: Double, v: Double): Double {
-        // Regner ut retningen vinden blåser mot
-        val directionBlowingTo = atan2(u, v)
-
-        // Gjør om til grader
-        var directionDegrees = Math.toDegrees(directionBlowingTo)
-
-        // Sørger for at vi får verdi mellom 0-360 grader
-        if (directionDegrees < 0) {
-            directionDegrees += 360.0
+        // Regner ut hvor sterk vinden er basert på øst-vest og nord-sør retning
+        fun calculateWindSpeed(u: Double, v: Double): Double {
+            // u er vindstyrke i øst-vest retning, v er vindstyrke i nord-sør retning
+            // Bruker pytagoras for å regne ut total vindstyrke
+            return sqrt(u * u + v * v)
         }
 
-        // Snur retningen for å få hvor vinden kommer fra
-        directionDegrees = (directionDegrees + 180.0) % 360.0
+        // Regner ut hvilken retning vinden kommer fra i grader (0-360)
+        fun calculateWindDirection(u: Double, v: Double): Double {
+            // Regner ut retningen vinden blåser mot
+            val directionBlowingTo = atan2(u, v)
 
-        return directionDegrees
+            // Gjør om til grader
+            var directionDegrees = Math.toDegrees(directionBlowingTo)
+
+            // Sørger for at vi får verdi mellom 0-360 grader
+            if (directionDegrees < 0) {
+                directionDegrees += 360.0
+            }
+
+            // Snur retningen for å få hvor vinden kommer fra
+            directionDegrees = (directionDegrees + 180.0) % 360.0
+
+            return directionDegrees
+        }
     }
-}
--- Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/data/repository/GribRepository.kt
+++ Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/data/repository/GribRepository.kt
@@ -3,7 +3,7 @@ package no.uio.ifi.in2000.team6.rakett_app.data.repository
 import no.uio.ifi.in2000.team6.rakett_app.data.GribDataSource
 import no.uio.ifi.in2000.team6.rakett_app.model.grib.Grib
 
-class GribRepository {
+    class GribRepository {
     private val gribDataSource = GribDataSource()
 
     suspend fun getGribData(lat: Double, longitude: Double): Grib? {
