a047dc90315a5999336dbeb2d562ed1e36d98ec4
ibrah <ibrahinu@ulrik.uio.no>
2025-03-27 16:10:37 +0100

    Lagde filen navigation.kt som håndterer navigasjonsbaren mellom de tre skjermene.

--- Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/MainActivity.kt
+++ Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/MainActivity.kt
@@ -1,17 +1,10 @@
 package no.uio.ifi.in2000.team6.rakett_app
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.mapbox.geojson.Point
-import com.mapbox.maps.extension.compose.MapboxMap
-import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
-import no.uio.ifi.in2000.team6.rakett_app.ui.home.HomeScreen
-
+import no.uio.ifi.in2000.team6.rakett_app.ui.Navigation
 import no.uio.ifi.in2000.team6.rakett_app.ui.theme.Rakett_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,12 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Rakett_AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-
+                Navigation()
             }
         }
     }
--- Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/ui/saved/SavedLocationScreen.kt
+++ Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/ui/saved/SavedLocationScreen.kt
@@ -1,2 +1,24 @@
 package no.uio.ifi.in2000.team6.rakett_app.ui.saved
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SavedLocationScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Lagrede Oppskytningssteder",
+            style = MaterialTheme.typography.headlineLarge,
+            textAlign = TextAlign.Center
+        )
+    }
+}
