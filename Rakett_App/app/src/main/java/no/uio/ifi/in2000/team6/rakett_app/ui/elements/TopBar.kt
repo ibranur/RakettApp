7d1dc52043538e4a5d82f640ca0c19c3be198439
haakonhk <haakonhk@ulrik.uio.no>
2025-04-03 21:33:34 +0200

    feat: Replace SmallTopAppBar with TopAppBar. deprecated

--- Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/ui/elements/TopBar.kt
+++ Rakett_App/app/src/main/java/no/uio/ifi/in2000/team6/rakett_app/ui/elements/TopBar.kt
@@ -5,14 +5,14 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,7 +25,7 @@ fun TopBar(
     isEmpty: Boolean = false
 ) {
     if (!isEmpty) {
-        SmallTopAppBar(
+        TopAppBar(
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = MaterialTheme.colorScheme.background,
                 titleContentColor = MaterialTheme.colorScheme.primary
@@ -51,8 +51,7 @@ fun TopBar(
 fun ScreenTitlePreview() {
     Scaffold(
         topBar = { TopBar(title = "Test", isEmpty = false) }
-    ) {
-            innerPadding ->
+    ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding))
         {
             Text(text = "Preview of Screen Title")
